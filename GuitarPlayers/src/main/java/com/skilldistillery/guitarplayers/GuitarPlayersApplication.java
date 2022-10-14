de27f7203d79979f993703e13c36eaf7c6716fdb
Josh Ingram <josh@Joshs-MacBook-Pro.local>
2022-10-14 16:28:51 -0600

    create boot project

---
+++ GuitarPlayers/src/main/java/com/skilldistillery/guitarplayers/GuitarPlayersApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.guitarplayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarPlayersApplication.class, args);
+	}
+
+}
