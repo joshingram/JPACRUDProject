2794cd6f54cf39d993648bdc35babcf4c562529a
Josh Ingram <josh@Joshs-MacBook-Pro.local>
2022-10-14 18:36:47 -0600

    db data filled

---
+++ DB/guitarplayerdb.sql
@@ -0,0 +1,56 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema guitarplayersdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `guitarplayersdb` ;
+
+-- -----------------------------------------------------
+-- Schema guitarplayersdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `guitarplayersdb` DEFAULT CHARACTER SET utf8 ;
+USE `guitarplayersdb` ;
+
+-- -----------------------------------------------------
+-- Table `player`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `player` ;
+
+CREATE TABLE IF NOT EXISTS `player` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `guitar_brand` VARCHAR(45) NULL,
+  `birth_place` VARCHAR(100) NULL,
+  `birth_country` VARCHAR(45) NULL,
+  `birth_day` DATE NULL,
+  `deceased_day` DATE NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `remarks` TEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS gpuser@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'gpuser'@'localhost' IDENTIFIED BY 'gpuser';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'gpuser'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `player`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `guitarplayersdb`;
+INSERT INTO `player` (`id`, `first_name`, `last_name`, `guitar_brand`, `birth_place`, `birth_country`, `birth_day`, `deceased_day`, `image_url`, `remarks`) VALUES (1, 'Eddie', DEFAULT, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+COMMIT;
+
